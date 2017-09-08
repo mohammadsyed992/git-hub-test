dede3c877bfb6912eca0e4f24400c38af82bbbaa
mohammad syed <mohammadsyed992@gmail.com>
2017-09-08 16:59:28 -0700

    Simple github commit

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main()
+{
+std::cout << "Hello World!" << std::endl;
+std::cout << "It's a wonderful life!" << std::endl;
+std::cout << "Make sure to take some time and enjoy it!" << std::endl;
+return 0;
+}
+
